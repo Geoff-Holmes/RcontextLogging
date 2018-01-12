cc4905bedfe30847083d709efd1fdee0632fc8b3
Geoff Holmes <geoffrholmes@gmail.com>
2018-01-12 10:29:16 +0000

    Add comments
    a ggplot save as part of the log_info object is all you need

--- test_script.R
+++ test_script.R
@@ -12,4 +12,8 @@ foo<-function(N=10)
     p1<-qplot(1:N, dat)
     ggsave(fig1_save_name)
     return(list(data=dat, fig1=p1, fig1_file=fig1_save_name))
+    # actually there is no need to save the figure or the data separately
+    # both can be recovered from the object p1 itself
+    # get figure with: p1
+    # get data with layer_data(p1) or ggplot_build(p1)
 }
